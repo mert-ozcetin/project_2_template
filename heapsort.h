9f252ac78596eb310c6f66864406d2dab8ff961c
mert-ozcetin <mertozcetin@ufl.edu>
2025-11-02 14:44:31 -0500

    Add heapSort functions to heapsort.h

--- heapsort.h
+++ heapsort.h
@@ -1,1 +1,10 @@
+#pragma once
+#include "mergesort.h"
 
+namespace sorting {
+void heapSort(std::vector<HealthRecord>& records, const Comparator& comparator);
+
+std::vector<HealthRecord> heapSortTopN(const std::vector<HealthRecord>& records, SortField field, bool descending = true, std::size_t topN = 50);
+std::vector<HealthRecord> heapSortTopN(const std::string& csvPath, SortField field, bool descending = true, std::size_t topN = 50);
+std::vector<HealthRecord> heapSortTopN(const std::string& csvPath, const std::string& field, bool descending = true, std::size_t topN = 50);
+} 
