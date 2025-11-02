ac718047fe6efdb4733f3688ae4ad418c6c2c6f4
mert-ozcetin <mertozcetin@ufl.edu>
2025-11-02 14:59:06 -0500

    Refactor Person struct and add BucketKey enum

--- main.h
+++ main.h
@@ -12,10 +12,10 @@ using namespace std;
 struct Person {
     int id = 0;
     int age = 0;
-    int bmi = 0;
+    double bmi = 0;
     int steps = 0;
-    int sleepHrs = 0;
-    int waterIntake = 0;
+    double sleepHrs = 0;
+    double waterIntake = 0;
     int caloriesConsumed = 0;
     int restingHeartRate = 0;
     int systolicBP = 0;
@@ -39,6 +39,19 @@ struct Bucket {
     list<int> idsList;
 };
 
+enum BucketKey {
+    MALE = 1,
+    FEMALE = 2,
+    SMOKER = 3,
+    NON_SMOKER = 4,
+    ALCOHOL = 5,
+    NON_ALCOHOL = 6,
+    FAMILY_HISTORY = 7,
+    NO_FAMILY_HISTORY = 8,
+    AT_RISK = 9,
+    NOT_AT_RISK = 10
+};
+
 //load data , will handle the initial data processing and create the complete hash map
 // the csv file is in resources
 //1 = male
@@ -59,23 +72,23 @@ unordered_map<int, Bucket> loadData(const string& path);
 
 //first search through bucket 1 and 2 which are male and female, respeactively.
 //determine which other buckets they are in and delete from data.
-void deleteId(int id, unordered_map<int, Bucket> &data){}
+void deleteId(int id, unordered_map<int, Bucket> &data);
 
 // not fully sure how we are going to implement this since the info would have to somehow be passed in
 // would also have to be hashed and inserted into each respective bucket
-void insertId(int id, unordered_map<int, Bucket> &data){}
+void insertId(int id, unordered_map<int, Bucket> &data);
 
 //similar to insert id with having to implement how we pass in the info as a param to the function.
 //we just override the prexisting id's info.
-void customizeId(int id, unordered_map<int, Bucket> &data){}
+void customizeId(int id, unordered_map<int, Bucket> &data);
 
 //generic display, we can just use male and female to find since they are guaranteed to be in one of them.
-void display(int id, unordered_map<int, Bucket> &data){}
+void display(int id, unordered_map<int, Bucket> &data);
 
 //our sorting algorithms - will sort based on risk factor not id.
-void merge_sort(forward_list<Person> &list) {}
-void quick_sort(vector<Person> &vec) {}
+void merge_sort(forward_list<Person> &list);
+void quick_sort(vector<Person> &vec);
 
 //each function will use our sorting alg on each data structure and return the right value;
-void nth_ind_highest(int n, unordered_map<int, Bucket> &data,int factor){}
-void nth_ind_lowest(int n, unordered_map<int, Bucket> &data,int factor){}
+void nth_ind_highest(int n, unordered_map<int, Bucket> &data,int factor);
+void nth_ind_lowest(int n, unordered_map<int, Bucket> &data,int factor);
